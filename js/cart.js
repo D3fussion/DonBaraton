9d1051addc568df866580dbe0bd8c1df78406fed
JUAN DIEGO QUIJADA CASTILLO <128547155+D3fussion@users.noreply.github.com>
2024-10-15 10:18:02 -0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -244,9 +244,17 @@ function limpiarError() {
 
 
 
+function eliminarDiacriticosEs(texto) {
+  return texto
+         .normalize('NFD')
+         .replace(/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,"$1")
+         .normalize();
+}
+
 document.getElementById('Buscar').addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent the default form submission
   var query = document.querySelector('#Buscar input[name="search"]').value;
+  query = eliminarDiacriticosEs(query);
   if (query) {
     window.location.href = "./product_list.html?" + encodeURIComponent(query.trim().replace(/\s+/g, '-'));
   }
